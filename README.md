9446f3e3d7849ec6f4692a2ce1118b5448d5e219
ckhung <chongki.hung@hkah.org.hk>
2021-01-27 11:40:54 +0800

    v1

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-this is a test"# node-simple-ws" 
+### Introduction
+
+[node-simple-ws](https://github.com/casualwriter/node-simple-ws) is simple web server program for node.js.
+
+* Single js file in about 60 lines code.
+* Self-contained, no depandence 
+* Portable, no installation needed
+* Simple SQL definition for general purpose
+* Connect via OLE DB driver for Oracle, MSSQL, MS Access, MySql, ODBC, etc..  
+
+### Installation & First Run
+
+The program is portable. 
+
+1. simple download ``node-simple-ws-v1.zip`` (it include node.exe, node-adodb, and simple-ws.js), 
+2. unzip it to a folder
+3. ``node.exe simple-ws`` to start the web service
+
+A sample web service will be started at port 9000, and the following web-service are available
+
+ http://localhost:9000/Categories
+ http://localhost:9000/Customers
+ http://localhost:9000/Product?key=hot
+ http://localhost:9000/Order?customer=Essen&country=Germany   
+
+
+### Coding for your web service
+
+The program is very simple in about 60 lines. You may read and modify the code.
+
+or just edit simple-ws.js for DB-CONNECTION and WEB-SERVICE-DEFINITION like
+
+~~~
+//=== Connect to datbase via OLEDB, e.g. MS Access 2003 (sample.mdb)
+var connection = ADODB.open( 'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=sample.mdb;' )
+
+
+//======================== WS Definition ========================
+var wsd = { name:'Webservice Definition' }
+
+// http://localhost:9000/Categories
+wsd.Categories = `select * from Categories`;
+
+// http://localhost:9000/Customers
+wsd.Customers	= `select * from Customers `;
+
+// e.g. http://localhost:9000/Product?key=hot
+wsd.Product	= `select * from Products where ProductName like '%{key}%' `;
+
+// e.g. http://localhost:9000/Order?customer=Essen&country=Germany
+wsd.Order	= `select a.* from Orders a, Customers b 
+              where a.CustomerID=b.CustomerID 
+                and b.CompanyName like '%{customer}%' 
+                and ShipCountry like '%{country}%' 
+            `;
+
+~~~
+
+
+### Release
+
+This progrm is released under [GPLv3](https://www.gnu.org/licenses/gpl-3.0.txt) (GNU通用公共許可證)
+
---
+++ source/simple-ws.js
@@ -0,0 +1,88 @@
+//=============================================================================================
+// This progrm is released under GPLv3 (GNU通用公共許可證)	https://www.gnu.org/licenses/gpl-3.0.txt
+//
+// Description: A simple web service program for gerneal purpose
+//
+// 2018/06: ck, initial version  
+// 2021/01: ck, release on https://github.com/casualwriter
+//=============================================================================================
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var port = 9000
+
+var ADODB = require('node-adodb');
+
+//=== Connect to datbase via OLEDB, e.g. MS Access 2003 (sample.mdb)
+var connection = ADODB.open( 'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=sample.mdb;' )
+
+// start web server at 9080
+http.createServer(function (request, response) {
+   try {
+     var reqUrl = url.parse(request.url, true)
+     
+     if (request.url != '/favicon.ico') {
+        console.log( '>> req. url =', request.url )
+        fs.appendFileSync( 'simple-ws.log', new Date().toISOString() + ": " + request.url + "\r\n" )
+     }
+      
+     // search wsd for valid path, and compose SQL
+     var sql=''
+      
+     for (var key in wsd) {
+        if (reqUrl.pathname == '/'+key) {
+          sql = wsd[key]
+          break;
+        }  
+     }
+     
+     for ( key in reqUrl.query ) sql = sql.replace( new RegExp('{'+key+'}','g'), reqUrl.query[key] );
+         
+     // run sql
+     if (sql > '' ) {
+        connection.query( sql )
+          .on('done', function(data) {
+            response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
+            response.write( JSON.stringify(data, function(k,v){return (v==null?"":(typeof v=='number'?''+v:v))}, '  ') );
+            response.end()
+            })
+          .on('fail', function(error) {
+             response.writeHead(404)    
+             response.write( 'Query Failed\n\nsql: '+sql+'\n\n' + JSON.stringify(error, null, '  ') );
+             response.end()          
+            });
+     } else {
+         response.writeHead(200)
+         response.write( 'Missing web service definition for '+ reqUrl.pathname + ' !' );
+         response.end()
+     }
+		 
+   } catch(e) {
+     response.writeHead(500)
+     response.end()
+     console.log(e.stack)
+   }
+	 
+}).listen(port)
+
+console.log("(NIS Interface) listening on port "+port)
+
+//======================== WS Definition ========================
+var wsd = { name:'Webservice Definition' }
+
+// http://localhost:9000/Categories
+wsd.Categories = `select * from Categories`;
+
+// http://localhost:9000/Customers
+wsd.Customers	= `select * from Customers `;
+
+// e.g. http://localhost:9000/Product?key=hot
+wsd.Product	= `select * from Products where ProductName like '%{key}%' `;
+
+// e.g. http://localhost:9000/Order?customer=Essen&country=Germany
+wsd.Order	= `select a.* from Orders a, Customers b 
+              where a.CustomerID=b.CustomerID 
+                and b.CompanyName like '%{customer}%' 
+                and ShipCountry like '%{country}%' 
+            `;
+
